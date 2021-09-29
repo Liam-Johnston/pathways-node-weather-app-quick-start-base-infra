80a3c75b696cb9305a07711ab1614e1fee9976ee
Liam Johnston <liam.johnston@contino.io>
2021-09-29 14:43:02 +1000

    Added more variable descriptions

--- modules/networking/variables.tf
+++ modules/networking/variables.tf
@@ -1,12 +1,20 @@
 variable "username" {
+  description = "The username of the user that is deploying this."
+  type        = string
 }
 
+
 variable "cidr_address" {
+  type        = string
+  description = "The CIDR Address of the VPC"
 }
 
 variable "region" {
+  type        = string
+  description = "The region to deploy in"
 }
 
 variable "az_names" {
-  type = list(string)
+  type        = list(string)
+  description = "A list of the names of the Avaliability Zones that this is getting deployed into"
 }
